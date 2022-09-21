f0c21bd418d985f8d7bcdb8731c4ba4ee03f8823
Mashhur <99575341+mashhurs@users.noreply.github.com>
2022-09-21 11:51:06 -0700

    Apply suggestions from code review
    Keep parallelism by default 4 in case if it affects to VRP.
    
    Co-authored-by: Ry Biesemeyer <yaauie@users.noreply.github.com>

--- plugindocs.rb
+++ plugindocs.rb
@@ -14,7 +14,7 @@ class PluginDocs < Clamp::Command
   option "--output-path", "OUTPUT", "Path to the top-level of the logstash-docs path to write the output.", required: true
   option "--main", :flag, "Fetch the plugin's docs from main instead of the version found in PLUGINS_JSON", :default => false
   option "--settings", "SETTINGS_YAML", "Path to the settings file.", :default => File.join(File.dirname(__FILE__), "settings.yml"), :attribute_name => :settings_path
-  option("--parallelism", "NUMBER", "for performance", default: 1) { |v| Integer(v) }
+  option("--parallelism", "NUMBER", "for performance", default: 4) { |v| Integer(v) }
   option "--skip-existing", :flag, "Don't generate documentation if asciidoc file exists"
 
   parameter "PLUGINS_JSON", "The path to the file containing plugin versions json"
